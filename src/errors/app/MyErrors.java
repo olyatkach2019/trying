45a757c1ce383ebd357614f79b8f75550673d485
olyatkach2019 <benyashka@yahoo.com>
2020-06-11 21:30:12 +0300

    y

--- src/errors/app/MyErrors.java
+++ src/errors/app/MyErrors.java
@@ -5,6 +5,7 @@ public class MyErrors {
     public static void main(String[] args) {
         System.out.println("second commit");
         System.out.println("second commit");
+        System.out.println("second commit");
 
     }
 }
